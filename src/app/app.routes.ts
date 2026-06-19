cc4bfd62182b93c92da73886a54833ae98c8a5e0
Piotr Synoś <piotr.synos@gmail.com>
2026-06-19 15:08:59 +0200

    Added new sites by Synoise.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { About } from './pages/about/about';
+import { Home } from './pages/home/home';
+import { Gallery } from './pages/gallery/gallery';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', component: Home
+  },
+  {
+    path: 'galeria', component: Gallery
+  },
+  {
+    path: 'about', component: About
+  }
+];
