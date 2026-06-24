a99b0d3ed42fd0faa60e845c7d78f0eccedc52f4
DESKTOP-UMSDHCL\User <achystiakow@gmail.com>
2026-06-24 13:30:18 +0200

    Zmiany

--- src/app/pages/multiplication/multiplication.ts
+++ src/app/pages/multiplication/multiplication.ts
@@ -6,4 +6,6 @@ import { Component } from '@angular/core';
   templateUrl: './multiplication.html',
   styleUrl: './multiplication.css',
 })
-export class Multiplication {}
+export class Multiplication {
+  data: any[] = ["10", 20, 30, 40, 50, 60, 70, 80, 90, 100];
+}
