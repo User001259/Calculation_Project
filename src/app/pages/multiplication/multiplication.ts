c03e1f4fd86c7f05eb3361343397c8a84b9af9a6
DESKTOP-UMSDHCL\User <csfor644@gmail.com>
2026-06-26 15:23:16 +0200

    Pozostaly zmiany

--- src/app/pages/multiplication/multiplication.ts
+++ src/app/pages/multiplication/multiplication.ts
@@ -8,35 +8,34 @@ import { CommonModule } from '@angular/common';
   templateUrl: './multiplication.html',
   styleUrls: ['./multiplication.css'],
 })
+
 export class Multiplication {
   data: number[] = [];
   name: string = 'Multiplication Table';
+  // data2: number[][] = [];
+  // size: number = 11;
 
-  data2: number[][] = [];
-  size: number = 11;
+  // getInnerArray(j: number): number[] {
+  //   let tempArray: number[] = [];
+  //   for (let i = 1; i < this.size; i++) {
+  //     tempArray.push(i * j);
+  //   }
+  //   return tempArray;
+  // }
 
-  getInnerArray(j: number): number[] {
-    let tempArray: number[] = [];
-    for (let i = 1; i < this.size; i++) {
-      tempArray.push(i * j);
-    }
-    return tempArray;
-  }
-
-  d: any = this.data;
+  d: number[] = this.data;
 
   ngOnInit() {
     for (let i = 1; i < 11; i += 1) {
       this.d.push(i);
     }
 
-    for (let i = 1; i < this.size; i++) {
-      this.data2.push(this.getInnerArray(i));
-    }
+    // for (let i = 1; i < this.size; i++) {
+    //   this.data2.push(this.getInnerArray(i));
+    // }
   }
 
   text: string = 'hello world';
-  text2: string = 'hello world';
 
   constructor() {}
 }
